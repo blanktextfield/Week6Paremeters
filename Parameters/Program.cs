03b184a8d7d2af57b3d61aafabe4ba00ff9ba55f
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:29:18 +0300

    Parameters

---
+++ Parameters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(50, 50);
+            DisplayString("Some random string");
+        }
+        public static void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }    public static void DisplayString(string someString)     
+        {  
+            Console.WriteLine(someString);
+        }  
+    }
+}
